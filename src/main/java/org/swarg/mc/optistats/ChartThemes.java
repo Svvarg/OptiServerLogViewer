d95a538709f579212b4f29473d6948c7943dd05d
Svvarg <stizerblack@gmail.com>
2021-11-17 12:05:01 +0300

    ChartThemes change colors

--- src/main/java/org/swarg/mc/optistats/ChartThemes.java
+++ src/main/java/org/swarg/mc/optistats/ChartThemes.java
@@ -39,14 +39,24 @@ public class ChartThemes {
         theme.setDrawingSupplier(new DefaultDrawingSupplier(
                 new Paint[] {
                     //определяем цвета самих линий графиков
-                    Color.decode("0x00FFCC"),//Green (Baseline)
+                    Color.decode("0x32CD32"),//limegreen 	#32CD32 	rgb(50,205,50)
                     Color.decode("0xA00000"),//Red
-                    Color.decode("0xFFFF00"),
-                    Color.decode("0xFF0000"),
-                    Color.decode("0xFFFF7F"), Color.decode("0x6681CC"),
-                    Color.decode("0xFF7F7F"), Color.decode("0xFFFFBF"),
-                    Color.decode("0x99A6CC"), Color.decode("0xFFBFBF"),
-                    Color.decode("0xA9A938"), Color.decode("0x2D4587")},
+                    Color.decode("0x00FFCC"),//Turquoise
+                    Color.decode("0x87CEEB"),//skyblue 	#87CEEB
+                    Color.decode("0xF4A460"),//sandybrown 	#F4A460 	rgb(244,164,96)
+                    Color.decode("0x6A5ACD"),//slateblue 	#6A5ACD 	rgb(106,90,205)
+                    Color.decode("0xFFFF7F"),
+                    Color.decode("0x4D85A7"),//
+                    Color.decode("0x6681CC"),
+                    Color.decode("0xFF7F7F"),
+                    Color.decode("0xFFFFBF"),
+                    Color.decode("0x99A6CC"),
+                    Color.decode("0xFFBFBF"),
+                    Color.decode("0xA9A938"),
+                    Color.decode("0xFF0000"),//
+                    Color.decode("0x8B4513"),//saddlebrown 	#8B4513 	rgb(139,69,19)
+                    //mediumseagreen 	#3CB371 	rgb(60,179,113)
+                },
                 new Paint[] {Color.decode("0xFFFF00"),
                     Color.decode("0x0036CC")},
                 new Stroke[] {new BasicStroke(2.0f)},
